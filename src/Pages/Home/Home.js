0f0c0470a39145128d33ff6bd9e12fba5bcc4fde
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-27 01:43:10 +0600

    added home page background image and content

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Home.css';
+import bg from '../../assets/images/bg.jpg';
 
 const Home = () => {
+  const myStyle = {
+    backgroundImage: `url(${bg})`,
+    minHeight: `calc(100vh - 74px)`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundBlendMode: 'overlay',
+    position: 'relative',
+  };
   return (
     <div>
-      <h1>Home Component</h1>
+      <div style={myStyle}>
+        <div className="header-content">
+          <h1>Explore BrainStormy</h1>
+          <p>
+            BrainStormy is an online learning and teaching marketplace with over
+            thousands of courses and millions of students. Learn programming,
+            Artificial Intelligence, data science and more!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
