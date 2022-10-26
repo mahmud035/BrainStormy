8c94a39a91764a6497d909bef76bbc2970095f39
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-26 15:15:46 +0600

    show course details data using CourseDetailsCard Component

--- src/Pages/Shared/CourseDetailsCard/CourseDetailsCard.js
+++ src/Pages/Shared/CourseDetailsCard/CourseDetailsCard.js
@@ -3,6 +3,10 @@ import './CourseDetailsCard.css';
 import Card from 'react-bootstrap/Card';
 import { FiCheck } from 'react-icons/fi';
 import { BsStarFill } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Image } from 'react-bootstrap';
+import { MdReviews } from 'react-icons/md';
+import { FaUserFriends } from 'react-icons/fa';
 
 const CourseDetailsCard = ({ course }) => {
   const {
@@ -14,7 +18,8 @@ const CourseDetailsCard = ({ course }) => {
     requirements,
     price,
     rating,
-    count,
+    description,
+    instructor,
   } = course;
 
   return (
@@ -22,17 +27,10 @@ const CourseDetailsCard = ({ course }) => {
       <Card.Img variant="top" src={image_url} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>{intro_description}</Card.Text>
-        <h5>What you'll learn</h5>
-        {learn.map((topic, index) => (
-          <>
-            <p key={index} className="mb-2">
-              <FiCheck className="me-2" /> {topic}
-            </p>
-          </>
-        ))}
 
-        <div className="pt-4">
+        <Card.Text>{intro_description}</Card.Text>
+
+        <div className="pb-4 pt-1">
           <small
             className="d-flex align-items-center gap-2 "
             style={{ fontSize: '13px' }}
@@ -49,9 +47,65 @@ const CourseDetailsCard = ({ course }) => {
           </small>
         </div>
 
-        <h5 className="pt-4">
+        <h5 className="pb-4">
           Price: <span className="fw-bold">{price}</span>
         </h5>
+
+        <div className="pb-4">
+          <h5>What you'll learn</h5>
+          {learn?.map((topic, index) => (
+            <>
+              <p key={index} className="mb-2">
+                <FiCheck className="me-2" /> {topic}
+              </p>
+            </>
+          ))}
+        </div>
+
+        <div className="pb-4">
+          <h5>Requirements</h5>
+          {requirements?.map((topic, index) => (
+            <>
+              <p key={index} className="mb-2">
+                <GoPrimitiveDot className="me-2" /> {topic}
+              </p>
+            </>
+          ))}
+        </div>
+
+        <div className="pb-4">
+          <h5>Description</h5>
+          <p>{description}</p>
+        </div>
+
+        <div className="pb-4">
+          <h5>Instructor</h5>
+          <h6>{instructor?.name}</h6>
+          <div className="mt-3 d-flex align-items-center gap-4">
+            <Image
+              roundedCircle
+              src={instructor?.img}
+              style={{ width: '110px', height: '110px' }}
+            ></Image>
+            <div>
+              <p className="mb-2">
+                <BsStarFill size={14} className="me-3" />
+                <small>
+                  {instructor?.instructor_rating}
+                  Instructor Rating
+                </small>
+              </p>
+              <p className="mb-2">
+                <MdReviews size={14} className="me-3" />
+                <small>{instructor?.reviews} Reviews</small>
+              </p>
+              <p className="mb-2">
+                <FaUserFriends size={14} className="me-3" />
+                <small>{instructor?.students} Students</small>
+              </p>
+            </div>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
